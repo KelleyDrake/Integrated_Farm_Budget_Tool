e34bd5cf504a940ec4a7ac6fda271f92e45b484d
Dow Drake <dowdrake@gmail.com>
2023-01-26 20:09:40 -0800

    Correct test value set when testing buggy code.

--- test_revenue.py
+++ test_revenue.py
@@ -7,4 +7,4 @@ def test_total_revenue():
     assert total_revenue == 9940729
 
     total_revenue = rev.total_revenue(.9, .9)
-    assert total_revenue == 8131679
+    assert total_revenue == 8139240
